ba0280150f3bc68312671f09f568db6c39381a5e
Tanvir Hasan <47018365+virza805@users.noreply.github.com>
2025-04-05 15:41:40 +0600

    need to start a package dev, right now

---
+++ info.php
@@ -0,0 +1,20 @@
+<?php
+/*
+my npmjs.com login details below
+user: 1mdalamin1 || 01mdalamin1@gmail.com
+pass: 
+vs extension = ES7+ React/Redux/React-Native snippets by dsznajder
+Choose package name |=> go to terminal and run this -> npm search package-name
+Creat package.json |=> go to terminal and run this -> npm init
+Upload package in https://www.npmjs.com/ |=> go to terminal and run this -> npm login
+package name same as folder name then upload index.js(must be export module js code) and package.json
+Upload package |=> go to terminal and run this -> npm publish
+
+#14 | 00:05s/11:08s |=> https://www.youtube.com/watch?v=F4DJl16cIV4&list=PL1Q1MIxW-vlRcjgWWhfngKiOVcA6lILb5&index=3
+
+
+
+
+*/
+
+
